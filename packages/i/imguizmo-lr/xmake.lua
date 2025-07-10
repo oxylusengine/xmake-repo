582bb7369671d89ccbfd9b8220384ca8bc1cc086
hatrickek <halimbayka@outlook.com>
2025-07-11 00:00:44 +0300

    add imguizmo, jolt, ktx, lua_static, slang, small_vector, spirv

---
+++ packages/i/imguizmo-lr/xmake.lua
@@ -0,0 +1,44 @@
+package("imguizmo-lr")
+    set_homepage("https://github.com/CedricGuillemet/ImGuizmo")
+    set_description("Immediate mode 3D gizmo for scene editing and other controls based on Dear Imgui")
+
+    add_urls("https://github.com/CedricGuillemet/ImGuizmo.git")
+
+    add_versions("v1.91.8-docking", "2310acda820d7383d4c4884b7945ada92cd16a47")
+
+    on_load(function (package)
+        local v = package:version()
+        package:add("deps", "imgui " .. v)
+    end)
+
+    on_install("macosx", "linux", "windows", "mingw", "android", "iphoneos", function (package)
+        local imgui = package:dep("imgui")
+        local configs = imgui:requireinfo().configs
+        if configs then
+            configs = string.serialize(configs, {strip = true, indent = false})
+        end
+        local xmake_lua = ([[
+            add_rules("mode.debug", "mode.release")
+            set_languages("c++14")
+
+            add_requires("imgui %s", {configs = %s})
+
+            target("imguizmo")
+                set_kind("static")
+                add_files("*.cpp")
+                add_headerfiles("*.h")
+                add_packages("imgui")
+        ]]):format(imgui:version_str(), configs)
+        io.writefile("xmake.lua", xmake_lua)
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            void test() {
+                ImGuiIO& io = ImGui::GetIO();
+                ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
+            }
+        ]]}, {configs = {languages = "c++11"}, includes = {"imgui.h", "ImGuizmo.h"}}))
+    end)
+package_end()
---
+++ packages/s/shader-slang/xmake.lua
@@ -0,0 +1,46 @@
+package("shader-slang")
+    set_homepage("https://github.com/shader-slang/slang")
+    set_description("Making it easier to work with shaders")
+    set_license("MIT")
+
+    if is_host("windows") then
+        add_urls("https://github.com/shader-slang/slang/releases/download/v$(version)/slang-$(version)-windows-x86_64.tar.gz",
+            {version = function (version) return version:gsub("v", "") end})
+
+        add_versions("v2025.10.4", "f4199d9cb32f93410444713adfe880da2b665a9e13f2f8e23fdbff06068a9ff3")
+    elseif is_host("linux") then
+        add_urls("https://github.com/shader-slang/slang/releases/download/v$(version)/slang-$(version)-linux-x86_64.tar.gz",
+            {version = function (version) return version:gsub("v", "") end})
+
+        add_versions("v2025.10.4", "c2edcfdada38feb345725613c516a842700437f6fa55910b567b9058c415ce8f")
+    end
+
+    on_install("windows|x64", "linux|x86_64", function (package)
+        os.cp("include/*.h", package:installdir("include"))
+
+        os.trycp("lib/*slang.*", package:installdir("lib"))
+        os.trycp("bin/*slang.*", package:installdir("lib"))
+
+        os.trycp("lib/*slang-glslang.*", package:installdir("lib"))
+        os.trycp("bin/*slang-glslang.*", package:installdir("lib"))
+
+        os.trycp("lib/*slang-glsl-module.*", package:installdir("lib"))
+        os.trycp("bin/*slang-glsl-module.*", package:installdir("lib"))
+
+        package:addenv("PATH", "bin")
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({ test = [[
+            #include <slang-com-ptr.h>
+            #include <slang.h>
+
+            void test() {
+                Slang::ComPtr<slang::IGlobalSession> global_session;
+                slang::createGlobalSession(global_session.writeRef());
+            }
+        ]] }, {configs = {languages = "c++17"}}))
+    end)
+
+package_end()
+
