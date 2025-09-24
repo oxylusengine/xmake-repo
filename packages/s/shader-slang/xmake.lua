12f4eb17d023eed7a95b75c2d4ed60d2def81b17
hatrickek <halimbayka@outlook.com>
2025-09-24 09:26:09 +0300

    slang - add macosx

--- packages/s/shader-slang/xmake.lua
+++ packages/s/shader-slang/xmake.lua
@@ -10,14 +10,39 @@ package("shader-slang")
         add_versions("v2025.10.4", "f4199d9cb32f93410444713adfe880da2b665a9e13f2f8e23fdbff06068a9ff3")
         add_versions("v2025.12.1", "02018cc923a46c434e23b166ef13c14165b0a0c4b863279731c4f6c4898fbf8e")
     elseif is_host("linux") then
-        add_urls("https://github.com/shader-slang/slang/releases/download/v$(version)/slang-$(version)-linux-x86_64.tar.gz",
-            {version = function (version) return version:gsub("v", "") end})
+        if is_arch("x86_64") then
 
-        add_versions("v2025.10.4", "c2edcfdada38feb345725613c516a842700437f6fa55910b567b9058c415ce8f")
-        add_versions("v2025.12.1", "8f34b98391562ce6f97d899e934645e2c4466a02e66b69f69651ff1468553b27")
+            add_urls("https://github.com/shader-slang/slang/releases/download/v$(version)/slang-$(version)-linux-x86_64.tar.gz",
+                {version = function (version) return version:gsub("v", "") end})
+
+            add_versions("v2025.10.4", "c2edcfdada38feb345725613c516a842700437f6fa55910b567b9058c415ce8f")
+            add_versions("v2025.12.1", "8f34b98391562ce6f97d899e934645e2c4466a02e66b69f69651ff1468553b27")
+        end
+
+        if is_arch("arm64") then
+            add_urls("https://github.com/shader-slang/slang/releases/download/v$(version)/slang-$(version)-linux-aarch64.tar.gz",
+                {version = function (version) return version:gsub("v", "") end})
+
+            add_versions("v2025.10.4", "c2edcfdada38feb345725613c516a842700437f6fa55910b567b9058c415ce8f")
+            add_versions("v2025.12.1", "d42edf9e778a63f532a25ae8b9f37e02ee7daa68e3e6e5d884b7ad0956ef253d")
+        end
+    elseif is_host("macosx") then
+        if is_arch("arm64") then
+            add_urls("https://github.com/shader-slang/slang/releases/download/v$(version)/slang-$(version)-macos-aarch64.tar.gz",
+                {version = function (version) return version:gsub("v", "") end})
+
+            add_versions("v2025.12.1", "205c6f61f6357ba3472551fa48d922f8220836c757d9b9059133938bdade02ae")
+        end
+
+        if is_arch("x86_64") then
+            add_urls("https://github.com/shader-slang/slang/releases/download/v$(version)/slang-$(version)-macos-x86_64.tar.gz",
+                {version = function (version) return version:gsub("v", "") end})
+
+            add_versions("v2025.12.1", "6c36eeb4dea30c614da90b970a998da8460c04688aa1ef60b673c23da424bb2a")
+        end
     end
 
-    on_install("windows|x64", "linux|x86_64", function (package)
+    on_install("windows|x64", "macosx|x86_64", "macosx|arm64", "linux|x86_64", "linux|arm64", function (package)
         os.cp("include/*.h", package:installdir("include"))
 
         os.trycp("lib/*slang.*", package:installdir("lib"))
