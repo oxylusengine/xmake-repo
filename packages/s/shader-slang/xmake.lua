590cf00c23622daf5c62d446a875eb5ce5f6fc54
hatrickek <halimbayka@outlook.com>
2025-09-24 10:35:45 +0300

    slang - only copy libslang

--- packages/s/shader-slang/xmake.lua
+++ packages/s/shader-slang/xmake.lua
@@ -45,8 +45,8 @@ package("shader-slang")
     on_install("windows|x64", "macosx|x86_64", "macosx|arm64", "linux|x86_64", "linux|arm64", function (package)
         os.cp("include/*.h", package:installdir("include"))
 
-        os.trycp("lib/*slang.*", package:installdir("lib"))
-        os.trycp("bin/*slang.*", package:installdir("lib"))
+        os.trycp("lib/libslang.*", package:installdir("lib"))
+        os.trycp("bin/libslang.*", package:installdir("lib"))
 
         os.trycp("lib/libslang-glslang.*", package:installdir("modules"))
         os.trycp("bin/libslang-glslang.*", package:installdir("modules"))
