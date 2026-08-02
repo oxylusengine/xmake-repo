d4631d3ef3c6e9a9d70bfe9d59f7c4ed2f496fc0
exdal <63502313+exdal@users.noreply.github.com>
2026-08-02 13:49:28 +0300

    sync install_resources

--- packages/o/oxylus/rules/resources.lua
+++ packages/o/oxylus/rules/resources.lua
@@ -1,6 +1,6 @@
 rule("install_resources")
     set_extensions(".png", ".ktx", ".ktx2", ".dds", ".jpg", ".mp3", ".wav", ".ogg",
-    ".otf", ".ttf", ".lua", ".txt", ".glb", ".gltf", ".oxasset", ".oxscene")
+    ".otf", ".ttf", ".lua", ".txt", ".glb", ".gltf", ".oxasset", ".oxscene", ".rml", ".rcss")
     before_buildcmd_file(function (target, batchcmds, sourcefile, opt)
         local output_dir = target:extraconf("rules", "@oxylus/install_resources", "output_dir") or ""
         local root_dir = target:extraconf("rules", "@oxylus/install_resources", "root_dir") or os.scriptdir()
